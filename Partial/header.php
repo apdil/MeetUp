5d5505f518e39450d2a9853b72a27cc67d962158
Apdil <apdil.aydinalp@gmail.com>
2017-07-11 16:14:16 +0200

    bootstrap 4.0 en place carousel Event ajouter

--- Partial/header.php
+++ Partial/header.php
@@ -2,22 +2,32 @@
 include_once '../Model/Personne.php';
  ?>
 <header class='navbar navbar-inverse margin0'>
-    <nav class='container-fluid'>
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">JUMP</a>
-        </div>
-        <ul class="nav mr-auto">
-            <li><a href="#">Carte</a></li>
-            <li><a href="#">BUZZ</a></li>
-            <li><a href="#">Message</a></li>
-        <?php if(isset($_SESSION['profil'])){ ?>
-        <li><a href='profileConnected.php'>Profil</a></li>
-        <li><a href='../Controller/deconectProcess.php'>Se Deconnecter</a></li>  
+    <nav class="navbar navbar-toggleable-md navbar navbar-inverse bg-primary bg-faded">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">JUMP</a>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">CARTE <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">BUZZ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Message</a>
+          </li>
+      <?php if(isset($_SESSION['profil'])){ ?>
+          <li class="nav-item"><a class='nav-link' href='profileConnected.php'>Profil</a></li>
+          <li class='nav-item'><a class='nav-link' href='../Controller/deconectProcess.php'>Se Deconnecter</a></li>  
         </ul>
-        <?php } else {?>
-        <li><a href='connexion.php'>Se connecter...</a></li>
-        <li><a href='inscription.php'>S'Inscrire</a></li>
+      <?php } else {?>
+          <li class='nav-item'><a class='nav-link' href='connexion.php'>Se connecter...</a></li>
+          <li class='nav-item'><a class='nav-link' href='inscription.php'>S'Inscrire</a></li>
         </ul>
-        <?php } ?>
-    </nav>
+      <?php } ?>
+      </div>
+  </nav>
 </header>
--- View/index.php
+++ View/index.php
@@ -11,9 +11,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Index</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">        
         <link rel="stylesheet" href='../style.css'>      
     </head>
     <body>
@@ -106,13 +104,58 @@
         </div>
         <main class='container'>
             <section class='container-fluid row'>
-            <h2 class='container row'>All evenement</h2>
-                <?php
-                foreach($events as $event){
-                    if(is_dir($event)) { continue; }
-                    $eventObject = $database->decodeFile('event', $event);
-                        include '../Partial/displayEventIndex.php';
-                    } ?>
+                <div class="container text-center my-3">
+                    <h2 class='container row'>All evenement</h2>
+                    <div class="row mx-auto my-auto">
+                        <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
+                            <div class="carousel-inner" role="listbox">
+                                <section class='container-fluid row'>
+                                    <?php
+                                    $i = 0;
+                                    foreach($events as $event){
+                                        if(is_dir($event)) { continue; }
+                                        $eventObject = $database->decodeFile('event', $event);
+                                            if($i == 0){
+                                                echo '<div class="carousel-item active">';
+                                            } else{
+                                                echo '<div class="carousel-item">';
+                                            }
+                                            include '../Partial/displayEventIndex.php';
+                                            echo '</div>';
+                                            $i++;
+                                        }?>
+                                </section>
+                                <!--<div class="carousel-item active">
+                                    <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=2">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=3">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=4">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=5">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block col-4 img-fluid" src="http://placehold.it/350x180?text=6">
+                                </div>-->
+                            </div>
+                            <a class="carousel-control-prev" href="#recipeCarousel" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#recipeCarousel" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                    <h4>Advances one slide at a time</h4>
+                </div>
             </section>
             <section class='container-fluid row'>
             <h2 class='container row'>Fete</h2>
@@ -217,5 +260,6 @@
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <script src='../jquery/carouselEvent.js'></script>
     </body>
 </html>
