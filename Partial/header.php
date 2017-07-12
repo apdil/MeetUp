55eb6e6ddc40d0ef27f0f14182cfae90cafb9df4
Apdil <apdil.aydinalp@gmail.com>
2017-07-12 23:32:05 +0200

    page connexion terminat

--- Partial/header.php
+++ Partial/header.php
@@ -6,12 +6,12 @@ include_once '../Model/Personne.php';
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">JUMP</a>
+      <a class="navbar-brand" href="../View/index.php">JUMP</a>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">CARTE <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">CARTE</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">BUZZ</a>
@@ -24,8 +24,7 @@ include_once '../Model/Personne.php';
           <li class='nav-item'><a class='nav-link' href='../Controller/deconectProcess.php'>Se Deconnecter</a></li>  
         </ul>
       <?php } else {?>
-          <li class='nav-item'><a class='nav-link' href='connexion.php'>Se connecter...</a></li>
-          <li class='nav-item'><a class='nav-link' href='inscription.php'>S'Inscrire</a></li>
+          <li class='nav-item'><a class='nav-link' href='connexion.php'>Profil</a></li>
         </ul>
       <?php } ?>
       </div>
--- View/connexion.php
+++ View/connexion.php
@@ -6,18 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href='../style.css'>      
     <title>connexion</title>
 </head>
 <body>
     <?php include_once '../Partial/header.php' ?>
-    <h2>Connexion</h2>
-    <form action='../Controller/connexionProcess.php' method='POST'>
-        <input type='text' name='login' id='login' placeholder='Login'>
-        <br/>
-        <input type='password' name='mdp' id='mdp' placeholder='Mdp'>
-        <input type='submit' value='Validez'>
-    </form>
-    <a href='../View/inscription.php'>Pas encore inscrit?</a>
+    <main class='container-fluid d-md-flex'>
+        <section class='container col-md-4 borderRight'>
+            <h2 class='d-flex justify-content-md-end justify-content-sm-center mb-6'>Connexion</h2>
+            <form action='../Controller/connexionProcess.php' method='POST'>
+                <div class="form-group">
+                    <input type='text' class="form-control width-input" name='login' id='login' placeholder='Login'>
+                </div>
+                <br/>
+                <div class="form-group">        
+                    <input type='password' class="form-control width-input" name='mdp' id='mdp' placeholder='Mdp'>
+                </div>
+                <input type='submit' class="btn btn-primary" value='Validez'>
+            </form>
+        </section>
+        <section class='container col-md-8'>
+            <h2 class='d-flex justify-content-md-start justify-content-sm-center mb-5'>Inscription</h2>
+            <div class='container d-flex'>
+                <div class='container col-md-7 d-flex flex-column justify-content-around'>
+                    <input type='submit' class="btn btn-primary mb-5" value='GOOGLE'>
+                    <input type='submit' class="btn btn-primary mb-5" value='TWITTER'>
+                    <input type='submit' class="btn btn-primary" value='FACEBOOK'>
+                </div>
+                <a href='../View/inscription.php'>
+                    <input type='submit' class="btn btn-primary" value='Je crée un compte'>
+                </a>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
